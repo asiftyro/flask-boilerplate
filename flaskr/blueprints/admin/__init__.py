31e2f958485ed0f9098a9194442781cd3bda931d
Asif R. Porosh <asif.tyro@gmail.com>
2022-01-18 21:05:22 +0600

    Admin layout update

--- flaskr/blueprints/admin/__init__.py
+++ flaskr/blueprints/admin/__init__.py
@@ -81,10 +81,10 @@ def post_update(id):
 @bp.route('/post/delete')
 @login_required
 def post_delete():
-    db = get_db()
-    posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' ORDER BY created DESC'
-    ).fetchall()
-    return render_template('views/admin/post/delete.html', posts=posts)
+    # db = get_db()
+    # posts = db.execute(
+    #     'SELECT p.id, title, body, created, author_id, username'
+    #     ' FROM post p JOIN user u ON p.author_id = u.id'
+    #     ' ORDER BY created DESC'
+    # ).fetchall()
+    return render_template('views/admin/post/delete.html')
